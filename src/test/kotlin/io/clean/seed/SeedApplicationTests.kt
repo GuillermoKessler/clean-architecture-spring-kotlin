952a223ae4052e9167706f4aa5a1306d68ecfb9a
GuillermoKessler <guillermokessler@gmail.com>
2022-04-01 19:15:32 -0300

    feat(init): init commit with base project

---
+++ src/test/kotlin/io/clean/seed/SeedApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.clean.seed
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SeedApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
